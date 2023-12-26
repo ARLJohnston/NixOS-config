5de35cf1fdfdf0eb30eaff60a4ae0d934f6836ef
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2023-12-26 22:22:10 +0000

    feat: add vim controls to tmux
    Note that these controls are not appropriate for vim use as they overload the <C-w> key commands

--- nixos/home_modules/foot.nix
+++ nixos/home_modules/foot.nix
@@ -28,7 +28,7 @@ in
         regular6 = "${colors.base0C}"; 
         regular7 = "${colors.base06}"; 
 
-        bright0 = "${colors.base04}"; 
+        bright0 = "${colors.base0D}"; 
         bright1 = "${colors.base08}"; 
         bright2 = "${colors.base0B}"; 
         bright3 = "${colors.base0A}"; 
@@ -40,16 +40,38 @@ in
     };
   };
 
-  #programs.tmux = {
-  #  enable = true;
-  #  keyMode = "vi";
-  #  #shortcut = "w";
-  #  prefix = "C-Space";
-  #  shortcut = "Space";
-  #  terminal = "tmux-256color";
-  #  plugins = with pkgs.tmuxPlugins; [
-  #    nord
-  #    tmux-fzf
-  #  ];
-  #};
+  programs.tmux = {
+    enable = true;
+    prefix = "C-w";
+    baseIndex = 1;
+    terminal = "screen-256color";
+    keyMode = "vi";
+    historyLimit = 5000;
+    escapeTime = 1;
+    clock24 = true;
+
+    plugins = with pkgs.tmuxPlugins; [
+      nord
+      sensible
+      tmux-fzf
+    ];
+
+    extraConfig = ''
+      bind v split-window -h
+      bind s split-window -v
+      bind w select-pane -t :.+
+      bind W select-pane -t :.-
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+      bind q kill-pane
+
+      set-option -g set-titles on
+    '';
+# May need to run this to get changes to take effect
+# > tmux kill-server
+# > tmux new -s test
+  };
+
 }
