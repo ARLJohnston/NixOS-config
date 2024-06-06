35e6052846ae2b7d3c4dd351a026cf03c218b72c
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-06-06 21:31:54 +0100

    feat(nix): update flake

--- flake.nix
+++ flake.nix
@@ -14,50 +14,41 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-colors = {
-      url = "github:misterio77/nix-colors";
-    };
+    nix-colors = { url = "github:misterio77/nix-colors"; };
 
     spicetify-nix = {
       url = "github:the-argus/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
     };
   };
 
   outputs = { self, nixpkgs, home-manager, emacs-overlay, ... }@inputs:
-  let
-    system = "x86_64-linux";
+    let
+      system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      inherit system;
+      pkgs = import nixpkgs { inherit system; };
+
+    in {
+      nixosConfigurations = {
+        thinkpad = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs system; };
+          modules = [ ./nixos/configuration.nix ];
+        };
+      };
+
+      homeConfigurations = {
+        alistair = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = { inherit inputs system; };
+          modules = [ ./nixos/home.nix ];
+        };
+      };
+
     };
-
-  in
-  {
-  nixosConfigurations = {
-    thinkpad = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs system; };
-      modules = [
-        ./nixos/configuration.nix
-      ];
-    };
-  };
-
-  homeConfigurations = {
-    alistair = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = { inherit inputs system; };
-      modules =
-        [
-          ./nixos/home.nix
-        ];
-    };
-  };
-
-  };
 }
