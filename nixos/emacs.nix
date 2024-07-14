172b529e1d592f84b488e71eb9b3ce627f9a16b4
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-07-14 15:28:06 +0100

    emacs: update config to work properly with direnv/eglot/corfu/yas

--- nixos/emacs.nix
+++ nixos/emacs.nix
@@ -1,89 +1,45 @@
 { pkgs, inputs, config, ... }:
 let
   my_emacs = (pkgs.emacsWithPackagesFromUsePackage {
-    package = pkgs.emacs-pgtk;
+    package = pkgs.emacs;
     config = ./init.el;
     defaultInitFile = true;
-    alwaysEnsure = false;
+    alwaysEnsure = true;
 
     extraEmacsPackages = epkgs:
       with epkgs; [
         all-the-icons
         all-the-icons-completion
         all-the-icons-dired
-        avy
-        cape
-        corfu
-        diminish
-        doom-themes
-        elfeed
-        envrc
-        evil
-        evil-collection
-        general
-        go-mode
-        imenu-list
-        jinx
-        kind-icon
+				imenu-list
         ligature
-        lsp-mode
-        lsp-treemacs
-        lsp-ui
-        magit
-        marginalia
-        nix-mode
+				envrc
+				kind-icon
         org
         org-auto-tangle
-        pdf-tools
-        rustic
-        sideline
-        sideline-flymake
+				pdf-tools
         tree-sitter-indent
-        undo-tree
-        vertico
+				yasnippet-capf
         vterm
-        vterm-toggle
-        yasnippet
-        zoxide
       ];
     override = final: prev: { NIX_CFLAGS_COMPILE = [ "-O3" "-march=native" ]; };
   });
 in {
   home.packages = with pkgs; [
-    clang
-    emacs-lsp-booster
-    maple-mono-NF
-    #lsp-servers
-    erlang-ls
-
-    godef
-    gopls
-
-    haskell-language-server
-
-    rust-analyzer
-
-    nixfmt
-
     #Org-preview
     texliveMedium
     tree-sitter
-    python3 # For extra lsp-mode features
+    python3
     emacs-all-the-icons-fonts
+    nuspell
+
+    (aspellWithDicts (dicts: with dicts; [ en en-computers ]))
     my_emacs
     enchant
   ];
 
   home.file = {
     ".emacs" = { source = ./init.el; };
-    ".aspell.conf" = { text = "dict-dir /run/current-system/sw/lib/aspell"; };
-    ".emacs.d/exwm/Xmodmap".text = ''
-      clear lock
-      clear control
-      keycode 66 = Control_L
-      add control = Control_L
-      add Lock = Control_R
-    '';
   };
 
   services = {
--- nixos/init.el
+++ nixos/init.el
@@ -12,18 +12,6 @@
 			(eval-print-last-sexp)))
 	(load bootstrap-file nil 'nomessage))
 
-;; If I uncomment the below then all packages will be evaluated on load because of nix/emacs things
-;; (straight-use-package 'use-package)
-;; (setq straight-use-package-by-default t)
-;; (require 'use-package)
-(require 'package)
-
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("gnu-devel" . "https://elpa.gnu.org/devel/")
-        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-        ("melpa" . "https://melpa.org/packages/")))
-
 (use-package general
   :config
   (general-evil-setup t)
@@ -82,15 +70,7 @@
     "bi" 'recentf
     "r" 'switch-to-buffer
     "w" (general-simulate-key "C-w")
-    "SPC" 'find-file
-    "f" 'avy-goto-char
-  )
-
-  (general-define-key
-   :states '(insert normal motion visual)
-   "C-f" 'avy-goto-char
-   "C-t" 'avy-goto-char-timer
-  )
+    "SPC" 'find-file)
 
   (general-define-key
    :states '(normal)
@@ -104,7 +84,16 @@
   :after general
 )
 
-(use-package avy)
+(use-package avy
+  :general
+  (general-define-key
+   :states '(insert normal motion visual)
+   "C-f" 'avy-goto-char
+   "C-t" 'avy-goto-char-timer)
+
+  (rune/leader-keys
+    "f" 'avy-goto-char)
+  )
 
 (use-package doom-themes
   :ensure t
@@ -145,10 +134,6 @@
 (use-package casual-dired
 	:straight (:type git :host github :repo "kickingvegas/casual-dired")
 )
-
-
-(use-package bind-key
-	:demand t)
 
 (unless (package-installed-p 'editorconfig)
 	(package-install 'editorconfig))
@@ -191,14 +176,35 @@
   (corfu-auto-delay 0.2)
   (corfu-popupinfo-delay '(0.4 . 0.2))
   (corfu-echo-documentation t)
-  :hook
-  (
-	  (after-init . global-corfu-mode)
-	  (global-corfu-mode . corfu-popupinfo-mode)
-  )
+  :init (global-corfu-mode)
+  :hook (global-corfu-mode . corfu-popupinfo-mode)
 	:diminish corfu-mode
 )
 
+(use-package cape
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative keys: M-p, M-+, ...
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+)
+
+(use-package yasnippet-capf
+  :ensure t
+  :after cape)
 
 (use-package kind-icon
   :after corfu
@@ -214,95 +220,27 @@
   (elfeed-search-filter "")
 	(elfeed-feeds
 		'(("https://xeiaso.net/blog.rss" nix)
-		 ("https://servo.org/blog/feed.xml" mis)
+		 ("https://servo.org/blog/feed.xml" misc)
+     ("https://blog.system76.com/rss.xml" misc)
 		 ("https://ferd.ca/feed.rss" misc)
 		 ("https://samoa.dcs.gla.ac.uk/events/rest/Feed/rss/123" research)
 		 ("https://xkcd.com/rss.xml" misc)))
   :general
   (rune/leader-keys
-    "e" '(lambda () (interactive) (elfeed-update) (elfeed))
-  )
+    "e" #'(lambda () (interactive) (elfeed-update) (elfeed))
+    )
+  (general-define-key
+   :keymaps 'eww-mode-map
+   "C-y" 'eww-copy-page-url
+   )
 	:defer t
 )
-
 
 (use-package markdown-mode
 	:defer t
 	)
 
-(use-package lsp-mode
-  :ensure t
-  :commands lsp
-  :hook
-  ((rust-mode . (lsp lsp-inlay-hints-mode))
-   (go-mode . (lsp lsp-inlay-hints-mode))
-   (kotlin-mode . lsp)
-   (c++-mode . lsp)
-   (lua-mode . lsp)
-   (prog-mode . lsp)
-   )
-  :custom
-  ;; cc-mode does not work well when following two settings are enabled.
-  (gc-cons-threshold 100000000)
-  (read-process-output-max (* 1024 1024)) ;; 1mb
-  (lsp-enable-on-type-formatting nil)
-  (lsp-enable-indentation t)
-  (lsp-diagnostics-provider :flymake)
-  (lsp-headerline-breadcrumb-icons-enable t)
-  (lsp-enable-snippet t)
-  (lsp-auto-guess-root t)
-  (lsp-idle-delay 0.1)
-  ;; (lsp-completion-provider :none)
-  (lsp-eldoc-render-all t)
-  (lsp-ui-doc-mode 1)
-  :bind
-  (:map lsp-mode-map
-		("C-c C-l" . lsp-execute-code-action)
-		("C-c r" . lsp-rename))
-  :config
-  (add-hook 'rust-mode-hook '(lambda() (add-hook 'before-save-hook 'lsp-format-buffer t t))))
-
-(use-package lsp-ui
-  :ensure t
-  :hook (lsp-mode-hook . lsp-ui-mode)
-  :custom
-  ;; doc
-  (lsp-ui-doc-enable nil)
-  :init
-  (defun toggle-lsp-ui-imenu ()
-    (interactive)
-	(let ((imenu-buffer (get-buffer lsp-ui-imenu-buffer-name)))
-	  (if imenu-buffer
-		  (progn
-			(lsp-ui-imenu-buffer-mode -1)
-			(kill-buffer lsp-ui-imenu-buffer-name)
-			(message "lsp-ui-imenu disabled"))
-		(progn
-		  (lsp-ui-imenu)
-		  (message "lsp-ui-imenu enabled")))))
-  :bind
-  (:map lsp-mode-map
-		("C-c C-i" . lsp-ui-peek-find-implementation)
-		("M-." . lsp-ui-peek-find-definitions)
-		("M-?" . lsp-ui-peek-find-references)
-		("C-c i" . toggle-lsp-ui-imenu)
-		("C-c C-s" . toggle-lsp-ui-sideline)))
-
-(use-package lsp-treemacs
-  :ensure t
-  :after lsp-mode)
-
-(use-package sideline)
-
-(use-package eldoc
-	:init
-	(global-eldoc-mode)
-  :diminish eldoc-mode
-  )
-
 (use-package nix-mode
-	;;:hook
-	;;(before-save . nix-mode-format)
   :init
   (add-hook 'nix-mode-hook
     (lambda () (add-hook 'before-save-hook 'nix-format-buffer nil t)))
@@ -367,27 +305,6 @@
 	 (java . t)
 	 ))
 
-;; ;;(use-package org-roam
-;; ;;	:init
-;; ;;	(setq org-roam-directory "~/org")
-;; ;;
-;; ;;	(setq org-roam-capture-templates
-;; ;;		'(("d" "default" plain
-;; ;;			"%?"
-;; ;;			:if-new (file+head "${slug}-%<%Y%m%d%H%M%S>.org"
-;; ;;				"#+title: ${title}\n")
-;; ;;			:unnarrowed t
-;; ;;			:jump-to-captured t)
-;; ;;		("m" "meeting" plain
-;; ;;			"%?"
-;; ;;			:if-new (file+head "${slug}-%<%Y%m%d%H%M%S>.org"
-;; ;;				"=================\n** Meeting %U\nAttendees:\n")
-;; ;;			:unnarrowed t
-;; ;;			:jump-to-captured t)))
-;; ;;)
-
-
-;;
 (use-package marginalia
 	:config
 	(marginalia-mode 1)
@@ -400,7 +317,6 @@
 	:hook
 	(marginalia-mode-hook . all-the-icons-completion-marginalia-setup)
 )
-
 
 (use-package evil
 	:custom
@@ -422,51 +338,26 @@
   :after evil
 	)
 
-(use-package jinx
-  :hook (emacs-startup . global-jinx-mode)
-  :bind (("M-$" . jinx-correct)
-         ("C-M-$" . jinx-languages))
-  :general
-  (general-define-key
-   "C-;" 'jinx-correct)
-  :diminish jinx-mode
-  )
-
 (use-package undo-tree
 	:init
 	(global-undo-tree-mode 1)
 	(evil-set-undo-system 'undo-tree)
-  :config
-	;;(undo-history-directory-alist '(("." . "~/.backups/")))
-  (kept-new-versions 6)
-  (kept-old-versions 2)
   :custom
   (undo-tree-history-directory-alist '(("." . "~/.backups")))
 	:ensure t
 	:diminish undo-tree-mode
-	:after evil
-	)
+	:after evil)
 
 (use-package vterm
-	:ensure t
-	)
+	:ensure t)
+
 (use-package vterm-toggle
   :init
 	(keymap-global-set "s-<return>" 'vterm-toggle)
   :ensure t
   )
 (keymap-global-set "s-c" 'calc)
-
-(use-package toggle-term
-  :bind (("M-o f" . toggle-term-find)
-         ("M-o t" . toggle-term-term)
-         ("M-o s" . toggle-term-shell)
-         ("M-o e" . toggle-term-eshell)
-         ("M-o i" . toggle-term-ielm)
-         ("M-o o" . toggle-term-toggle))
-  :config
-    (setq toggle-term-size 25)
-    (setq toggle-term-switch-upon-toggle t))
+(keymap-global-set "C-c C-c" 'compile)
 
 (use-package zoxide
 	:general
@@ -484,7 +375,7 @@
 (use-package pdf-tools
 	:init
 	(pdf-loader-install)
-	(add-hook 'pdf-view-mode-hook '(lambda () (display-line-numbers-mode -1)))
+	(add-hook 'pdf-view-mode-hook #'(lambda () (display-line-numbers-mode -1)))
 	(add-hook 'pdf-view-mode-hook 'pdf-view-midnight-minor-mode)
 	(add-hook 'pdf-mode-hook #'(lambda () (interactive) (display-line-numbers-mode -1)))
 	:defer t
@@ -499,62 +390,12 @@
 	:defer t
 	)
 
-(use-package lsp-haskell
-	:defer t
-	)
-
-(use-package haskell-mode
-	:init
-	(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-	:defer t
-	)
+(use-package gorepl-mode
+  :hook
+  (go-mode . gorepl-mode))
 
 (use-package envrc
-  :diminish envrc-mode
 	:hook (after-init . envrc-global-mode))
-
-(use-package rustic
-  :defer t
-  :mode ("\\.rs?$" . rustic-mode)
-)
-
-(use-package tree-sitter-indent)
-
-(use-package gleam-ts-mode
-  :load-path "~/.emacs.d/gleam-mode"
-  )
-
-(add-hook 'gleam-mode-hook
-          (lambda () (add-hook 'before-save-hook 'gleam-ts-format nil t)))
-
-(defun lsp-booster--advice-json-parse (old-fn &rest args)
-  "Try to parse bytecode instead of json."
-  (or
-   (when (equal (following-char) ?#)
-     (let ((bytecode (read (current-buffer))))
-       (when (byte-code-function-p bytecode)
-         (funcall bytecode))))
-   (apply old-fn args)))
-(advice-add (if (progn (require 'json)
-                       (fboundp 'json-parse-buffer))
-                'json-parse-buffer
-              'json-read)
-            :around
-            #'lsp-booster--advice-json-parse)
-
-(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
-  "Prepend emacs-lsp-booster command to lsp CMD."
-  (let ((orig-result (funcall old-fn cmd test?)))
-    (if (and (not test?)                             ;; for check lsp-server-present?
-             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
-             lsp-use-plists
-             (not (functionp 'json-rpc-connection))  ;; native json-rpc
-             (executable-find "emacs-lsp-booster"))
-        (progn
-          (message "Using emacs-lsp-booster for %s!" orig-result)
-          (cons "emacs-lsp-booster" orig-result))
-      orig-result)))
-(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
 
 (use-package ligature
   :config
@@ -565,5 +406,50 @@
                                        ":=" ":-" ":+" "<*" "<*>" "*>" "<|" "<|>" "|>" "+:" "-:" "=:" "<******>" "++" "+++"))
   ;; Enables ligature checks globally in all buffers. You can also do it
   ;; per mode with `ligature-mode'.
-  :init
-  (global-ligature-mode t))
+  :hook (prog-mode . ligature-mode)
+  )
+
+(use-package docker
+  :ensure t
+  :general
+  (rune/leader-keys
+		"d" 'docker)
+  )
+
+(use-package eglot
+	:hook (prog-mode . eglot-ensure)
+  :general
+  (rune/leader-keys
+   :keymaps 'prog-mode-map
+   "lr" 'eglot-rename
+  )
+	:bind
+	("M-RET" . eglot-code-actions)
+  :custom
+  (gc-cons-threshold 100000000)
+  (read-process-output-max (* 1024 1024)) ;; 1mb
+  :config
+    (setq-default eglot-workspace-configuration
+		'((:gopls .
+			  (
+			    (staticcheck . t)
+			    (completeUnimported          . t)
+			    (usePlaceholders             . t)
+			    (expandWorkspaceToModule     . t)
+			    ))))
+  :demand t
+  :hook (eglot-managed-mode . (lambda ()
+
+	(setq-local completion-at-point-functions
+				(list (cape-capf-super
+					   #'eglot-completion-at-point #'yasnippet-capf)))))
+  :after yasnippet
+)
+
+(use-package eldoc
+	:init
+	(global-eldoc-mode)
+  :diminish eldoc-mode
+  )
+
+
