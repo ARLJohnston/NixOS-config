653a007f138dff1c2b100aedf1cfe1c9057d43a8
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-09-04 16:04:13 +0100

    feat(home): add lorri
    * nixos/home.nix:

--- nixos/home.nix
+++ nixos/home.nix
@@ -28,6 +28,8 @@ in {
     };
   };
 
+  services.lorri.enable = true;
+
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [ "spotify" ];
 
@@ -49,136 +51,27 @@ in {
     ./home_modules/librewolf.nix
   ];
 
-  programs.spicetify =
-    let spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-    in {
-      enable = true;
-      enabledExtensions = with spicePkgs.extensions; [
-        adblock
-        hidePodcasts
-        shuffle # shuffle+ (special characters are sanitized out of extension names)
-      ];
-      theme = spicePkgs.themes.catppuccin;
-      colorScheme = "mocha";
-    };
-
-  home.packages = with pkgs; [ lsd tree zoxide pamixer brightnessctl ];
-
-  # services.hyprpaper = {
-  #   enable = true;
-
-  #   settings = {
-  #     ipc = "off";
-  #     splash = false;
-  #     preload = [ "~/wallpaper.png" ];
-  #     wallpaper = [ "eDP-1,~/wallpaper.png" ];
+  # programs.spicetify =
+  #   let spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+  #   in {
+  #     enable = true;
+  #     enabledExtensions = with spicePkgs.extensions; [
+  #       adblock
+  #       hidePodcasts
+  #       shuffle # shuffle+ (special characters are sanitized out of extension names)
+  #     ];
+  #     theme = spicePkgs.themes.catppuccin;
+  #     colorScheme = "mocha";
   #   };
-  # };
 
-  # wayland.windowManager.hyprland = {
-  #   enable = true;
-  #   # xwayland.enable = true;
-  #   package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-  #   systemd.enable = true;
-
-  #   plugins = [
-  #     # inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo
-  #   ];
-
-  #   extraConfig = ''
-  #     bind=,Print,exec,grim -t png -g "$(slurp)" ~/Pictures/$(date +%Y-%m-%d_%H-%m-%s).png
-  #   '';
-
-  #   settings = {
-  #     "$mod" = "SUPER";
-
-  #     general = {
-  #       gaps_in = 2;
-  #       gaps_out = 2;
-  #     };
-
-  #     animations = { enabled = false; };
-
-  #     misc = {
-  #       disable_hyprland_logo = true;
-  #       disable_autoreload = true;
-  #       enable_swallow = true;
-  #     };
-
-  #     binds = {
-  #       workspace_back_and_forth = true;
-  #       allow_workspace_cycles = true;
-  #     };
-
-  #     cursor = { hide_on_key_press = true; };
-
-  #     device = {
-  #       name = "tpps/2-ibm-trackpoint";
-  #       sensitivity = -0.3;
-  #     };
-
-  #     input = {
-  #       kb_layout = "gb";
-  #       kb_options = "ctrl:nocaps";
-  #     };
-
-  #     exec-once = [
-  #       "swaylock -i ~/lock.png"
-  #       # "emacs --fg-daemon"
-  #       "[workspace special:magic] keepassxc ~/Passwords.kbdx"
-  #     ];
-
-  #     binde = [
-  #       "$mod, H, resizeactive, -10 0"
-  #       "$mod, L, resizeactive, 10 0"
-  #       ",XF86AudioRaiseVolume, exec, pamixer -i 5"
-  #       ",XF86AudioLowerVolume, exec, pamixer -d 5"
-  #       ",XF86AudioMute, exec, pamixer --toggle-mute"
-  #       ",XF86MonBrightnessUp, exec, brightnessctl set +5%"
-  #       ",XF86MonBrightnessDown, exec, brightnessctl set 5%-"
-  #     ];
-
-  #     bindm = [ "$mod, mouse:272, movewindow" "$mod, mouse:273, resizewindow" ];
-
-  #     bind = [
-  #       "$mod, W, exec, librewolf"
-  #       "$mod, D, exec, wofi --show drun"
-  #       "$mod, X, exec, ~/.config/rofi/powermenu.sh"
-  #       "$mod, Q, killactive"
-  #       "$mod, F, fullscreen, 1"
-  #       "$mod, TAB, workspace, previous"
-
-  #       "$mod, P, togglespecialworkspace, magic"
-  #       "$mod SHIFT, P, movetoworkspace, special:magic"
-  #       "$mod, Space, togglefloating"
-
-  #       "$mod, k, movefocus, u"
-  #       "$mod, j, movefocus, d"
-
-  #       "$mod, 1, workspace, 1"
-  #       "$mod SHIFT, 1, movetoworkspace, 1"
-  #       "$mod, 2, workspace, 2"
-  #       "$mod SHIFT, 2, movetoworkspace, 2"
-  #       "$mod, 3, workspace, 3"
-  #       "$mod SHIFT, 3, movetoworkspace, 3"
-  #       "$mod, 4, workspace, 4"
-  #       "$mod SHIFT, 4, movetoworkspace, 4"
-  #       "$mod, 5, workspace, 5"
-  #       "$mod SHIFT, 5, movetoworkspace, 5"
-  #       "$mod, 6, workspace, 6"
-  #       "$mod SHIFT, 6, movetoworkspace, 6"
-  #       "$mod, 7, workspace, 7"
-  #       "$mod SHIFT, 7, movetoworkspace, 7"
-  #       "$mod, 8, workspace, 8"
-  #       "$mod SHIFT, 8, movetoworkspace, 8"
-  #       "$mod, 9, workspace, 9"
-  #       "$mod SHIFT, 9, movetoworkspace, 9"
-  #       "$mod, 0, workspace, 10"
-  #       "$mod SHIFT, 0, movetoworkspace, 10"
-
-  #     ];
-  #   };
-  # };
+  home.packages = with pkgs; [
+    lsd # better ls
+    tree
+    zoxide
+    pamixer
+    brightnessctl
+    direnv
+  ];
 
   programs.git = {
     enable = true;
@@ -191,15 +84,13 @@ in {
     };
   };
 
-  home.sessionVariables = {
-    NIXOS_OZONE_WL = 1;
-    MOZ_ENABLE_WAYLAND = 1;
-    # XDG_CURRENT_DESKTOP = "Hyprland";
-    # XDG_SESSION_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-    GDK_BACKEND = "wayland,x11";
-    QT_QPA_PLATFORM = "wayland";
-  };
+  # home.sessionVariables = {
+  #   NIXOS_OZONE_WL = 1;
+  #   MOZ_ENABLE_WAYLAND = 1;
+  #   XDG_SESSION_TYPE = "wayland";
+  #   GDK_BACKEND = "wayland,x11";
+  #   QT_QPA_PLATFORM = "wayland";
+  # };
 
   home.file = {
     ".config/cosmic/com.system76.CosmicComp/v1/xkb_config".text = ''
